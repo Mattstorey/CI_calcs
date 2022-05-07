82b5e0b2048d9e2d94ba8e902434e67920b5e900
Mattstorey <mathstorey@gmail.com>
2022-05-07 18:57:10 +1200

    Add unit tests for calculator

---
+++ calculator.py
@@ -0,0 +1,30 @@
+"""
+Calculator module library containing basic math functions.
+"""
+
+
+def add(first_term: int, second_term: int) -> int:
+    """
+    Add two numbers.
+    """
+    return first_term + second_term
+
+
+def subtract(first_term: int, second_term: int) -> int:
+    """
+    Subtract two numbers.
+    """
+    return first_term - second_term
+
+
+# def multiply(first_term: int, second_term: int) -> int:
+#     """
+#     Multiply two numbers.
+#     """
+#     return first_term * second_term
+
+# def divide(first_term: int, second_term: int) -> int:
+#     """
+#     Divide two numbers
+#     """
+#     return first_term / second_term
